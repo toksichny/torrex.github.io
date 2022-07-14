84124ad7b80771a57de8b397fe55462e928125c6
netcool <51483807+toksichny@users.noreply.github.com>
2022-07-14 18:00:49 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,7 @@ import os, sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from application import create_app
-
-app = create_app()
+from main import app
 
 if __name__ == "__main__":
     app.run()
